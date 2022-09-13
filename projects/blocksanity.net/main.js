5e5b9201a105d52f3e8a8183bf536d8c50ed3a96
CrazyCowMM <38326866+CrazyCowMM@users.noreply.github.com>
2022-09-12 17:50:47 -0700

    bs.net bucket push

---
+++ projects/blocksanity.net/main.js
@@ -0,0 +1,219 @@
+var mapsList = [
+    {
+        "title": "The Secret Island",
+        "shortDescription": "Explore a stunning minigame filled island",
+        "longDescription": "This is the Secret Island! (https://i.imgur.com/CZ1dMV0.png)(https://www.youtube.com/embed/v_H9utcYumo)(https://www.youtube.com/embed/BeCDsT-8TUM) testing texted added after",
+        "version": "1.13.2",
+        "download": "https://www.minecraftmaps.com/adventure-maps/the-secret-island/download",
+        "creators": "",
+        "creatorIcons": "",
+        "images": ["https://static.planetminecraft.com/files/resource_media/screenshot/1927/screen-1-1562541561.png",
+            "https://static.planetminecraft.com/files/resource_media/screenshot/1927/screen-2-1562541585.png",
+            "https://static.planetminecraft.com/files/resource_media/screenshot/1927/screen-9-1562541596.png",
+            "https://static.planetminecraft.com/files/resource_media/screenshot/1927/screen-12-1562541612.png",
+            "https://static.planetminecraft.com/files/resource_media/screenshot/1927/screen-3-1562541624.png",
+            "https://static.planetminecraft.com/files/resource_media/screenshot/1927/screen-4-1562541651.png",
+            "https://static.planetminecraft.com/files/resource_media/screenshot/1927/screen-5-1562541659.png",
+            "https://static.planetminecraft.com/files/resource_media/screenshot/1927/dungeon-escape-1562541687.png"],
+        "icon": "https://static.planetminecraft.com/files/resource_media/screenshot/1928/island-thumbnail-1562716697.png",
+        "url": "the_secret_island"
+    },
+    {
+        "title": "Water Lucky Block Race",
+        "shortDescription": "Race your friends to the end of the track",
+        "longDescription": "Break your way through the ocean in this fun, water inspired Lucky Block Race!! <br> With spectacular builds and custom trades you are sure to have a great time. <br> (https://www.youtube.com/embed/48JSW1se-M0)(https://www.youtube.com/embed/Tlgx3vwjRPs)",
+        "version": "1.8.9",
+        "download": "https://www.dropbox.com/s/kv0c3r80d50tdbe/Water%20Lucky%20Block%20Race%201.8.9.zip?dl=0",
+        "creators": "",
+        "creatorIcons": "",
+        "images": ["https://i.ibb.co/pwjjX6J/2019-03-30-20-34-48.png",
+            "https://i.ibb.co/Wy2vHsg/2019-03-30-20-35-01.png",
+            "https://i.ibb.co/sJmHx98/2019-03-30-20-35-11.png",
+            "https://i.ibb.co/NyP1344/lbwpic4.jpg",
+            "https://i.ibb.co/KKYYGc2/lbwpic3.jpg",
+            "https://i.ibb.co/31kH96P/lbwpic1.jpg"],
+        "icon": "https://i.ibb.co/B6H9ntt/wlbl.jpg",
+        "url": "water_lucky_block_race"
+    },
+    {
+        "title": "Mission Cold",
+        "shortDescription": "Embark on an epic journey to save santa!",
+        "longDescription": "From the same creators that brought you Parkour Island, and the ongoing Reality of Dreams series, comes Mission Cold. <br>Mission Cold is a 1-10 player adventure map, centered around the idea of racing your friends. Featuring voice acting, music, custom sounds, models, textures, and really cool hallways, this map is sure to be a blast. <br>Story<br> After most employees have already left to spend their remaining time with their families, you and any others arrive at the TRG headquarters, to be briefed on your secret mission to save the most important man in minecraftia, from a killer storm. <br> Gameplay tips <br> <ul><li> In case you drive too far into an unloaded area, feel free to either stop your vehicle and let the chunks load in- or press F3+A to reload them instantly. </li><li>Hostile mobs = Music for the most part </li><li> Friendly mobs = Voices for the most part </li><li> Click all signs, who knows, you might discover something hidden. </li>",
+        "version": "1.13.2",
+        "download": "https://miniurl.pw/28aYR",
+        "creators": "",
+        "creatorIcons": "",
+        "images": ["https://i.ibb.co/hKcybHQ/hyXYSQW.jpg",
+            "https://i.ibb.co/zRW7vLk/YQA6E1Z.png",
+            "https://i.ibb.co/ydLHHDb/4V3WMm4.png",
+            "https://i.ibb.co/wJ4H6R2/zMnslAY.png",
+            "https://i.ibb.co/JjMyfsW/ytldsoP.png"],
+        "icon": "https://i.imgur.com/Jrb6679.jpg",
+        "url": "mission_cold"
+    },
+    {
+        "title": "Pixel Party",
+        "shortDescription": "One island, 9 unique minigames, and tons of fun!",
+        "longDescription": "Pretend to be on a floating island. Just, a floating island! A floating island with only one goal: To bring you and your friends a lot of fun! <br> Shouldn't be that hard, as this island is perfectly designed for that. First, choose how many rounds you want to play. Then, per round you can select which minigame you would like to play. <br>Maybe you find Lava Floor fun, or maybe you prefer Bow Circle? From zombies and traitors to Boomballs and raining anvils. It's all up to you and your friends! <br> <br>FEATURES<br>This map contains:<br> 9 unique minigames:<br> Boomball<br> Nightmare <br> Sneaky Snipers <br> Anivil Attack  <br> Border Battle <br> King Of The Potato <br> Mine Diamonds <br> Bow Circle <br> Lava Floor <br>A unique build <br>A very special floor <br>A parkour course <br>Support for every amount of people, although you will need 2 or more players to play the map <br>An advanced reset system <br>Got kicked, or tuned in lately? No problem! The map will check if it is needed to modify your score.<br>Endless fun <br> <br>NOTES <br>This map requires multiple people. The map won't start/will directly reset if there's only one player.",
+        "version": "1.13.2",
+        "download": "http://www.mediafire.com/file/su6my5rhgvvhhby/Pixel_Party.zip/file",
+        "creators": "",
+        "creatorIcons": "",
+        "images": ["https://i.ibb.co/cT0Cw2P/2x74o7s.png",
+            "https://i.ibb.co/0yDVkz9/LIvF9n3.png",
+            "https://i.ibb.co/8Ywg0wt/xc9mSi3.png",
+            "https://i.ibb.co/mGpLFpW/ZzlNATf.png",
+            "https://i.ibb.co/PhNWDYW/pMQJk1p.png"],
+        "icon": "https://i.ibb.co/XJyfXMn/pp.png",
+        "url": "pixel_party"
+    },
+    {
+        "title": "Parkour Island",
+        "shortDescription": "Parkour to the top of the island!",
+        "longDescription": "The Parkour Island isn't a small 10 minute map. This Island is much bigger than the thumbnail shows. You can play it by yourself or with friends if you wish. You start from the bottom and through three rings you make your way to the top of a mountain in the middle of the island. What's at the top? Only you can find out!<br><br> There are no special block effects in this map, but we do suggest that: <br>1. You should set your render distance to above 12 chunks. <br>2. You should have your in game volume up, as there are little bits of music here and there. <br>3. You don't give up.<br><br> Not only is the parkour on this island challenging at times but you may not know exactly where to go. If a section seems impossible to complete or you just can't find the answer to how to complete it: try locating where you want to go and work your way back to where you are.<br><br> Parkour Island is Team Blocksanity's first parkour map. We hope you have a pleasant time playing. Rules and Credits are displayed within the map itself.",
+        "version": "1.13.2",
+        "download": "http://adfoc.us/46673469417988",
+        "creators": "",
+        "creatorIcons": "",
+        "images": ["https://i.ibb.co/zbq0Ys6/S1ptam5.png",
+            "https://i.ibb.co/qnbnCTm/7kA5Q0z.jpg",
+            "https://i.ibb.co/7vQvKkb/RrVxRIb.png",
+            "https://i.ibb.co/fNfhk2B/yPhoRrF.png",
+            "https://i.ibb.co/Y3BCTF8/qmZ03H5.png"],
+        "icon": "https://i.imgur.com/9iaIEa5.jpg",
+        "url": "parkour_island"
+    },
+    {
+        "title": "Blockbrawl: The Game",
+        "shortDescription": "1v1 PvP with custom items and weapons!",
+        "longDescription": "One saturday morning, the map making team Blocksanity saw a game called \"STICK FIGHT: THE GAME\" on youtube. After a bit of talking and throwing a few blocks together, we made a very similar map for your enjoyment.<br><br> BLOCK BRAWL: THE GAME is a 1v1 deathmatch game, with custom weapons, custom victory messages, and lots of cool arenas that can be selected to play on.<br><br> The game will not let you play with a single player, and although it is compatible with 11+ players, only two players will be fighting. The game randomly selects two players to fight and leaves everyone else to watch in a cool room. Spectators will always get to choose what map the fighters play on, unless there are no spectators... in which case, one of the players would get to choose the map. Weapons spawn randomly in four locations across the map, and additionally there are secret items that players can unlock by inputting codes that can be found on youtube channels. Rounds go by quick, oh and by the way, Cooldowns on weapons are DECREASED just enough so that you can spam weapons.<br><br> The image below with the wooden floor and wooden walls is the \"Spectator Lounge,\" where players can watch the action unfold-- on what appears to be a 2D Platformer. There are currently five arenas for players to play on, but we are planning on giving the map a bit, adding LOTS of arenas and weapons to it.<br><br> Consider this first version of the game a demo for what's to come. This being said, feel free in the comments below to give Map Suggestions and/or Item Ideas.",
+        "version": "1.12.2",
+        "download": "http://adfoc.us/46673469265602",
+        "creators": "",
+        "creatorIcons": "",
+        "images": ["http://blocksanity.net/maps/bbpic1.png",
+            "http://blocksanity.net/maps/bbpic2.png",
+            "http://blocksanity.net/maps/bbpic3.png",
+            "http://blocksanity.net/maps/bbpic4.png",
+            "http://blocksanity.net/maps/bbpic5.png"],
+        "icon": "http://blocksanity.net/maps/blockbrawl.png",
+        "url": "blockbrawl"
+    }
+];
+//const mapPage = require('mapPage')
+
+// function svgScroll() {
+//   for (var path in document.getElementsByTagName("path")) {
+//     //console.log(window.innerHeight)
+//     //console.log("SVG Element Offset: " + dw_getPageOffsets(document.getElementsByTagName("path")[path]) + " Window Top: " + dw_getScrollOffsets().y + " Window bottom: " + (dw_getScrollOffsets().y + window.innerHeight))
+//     if( dw_getScrollOffsets().y < dw_getPageOffsets(document.getElementsByTagName("path")[path]) && dw_getScrollOffsets().y + window.innerHeight > dw_getPageOffsets(document.getElementsByTagName("path")[path])) {
+//         //console.log(dw_getPageOffsets(document.getElementsByTagName("path")[path]))
+//         document.getElementsByTagName("path")[path].setAttribute("transform","translate(" + -(dw_getScrollOffsets().y%window.innerHeight)/5 + ", 0)")
+//       }
+//   }
+//     setTimeout(svgScroll,100)
+// }
+
+// function dw_getPageOffsets(el) {
+//     var sOff = dw_getScrollOffsets(), left = 0, top = 0, props;
+//
+//     if ( el.getBoundingClientRect ) {
+//         props = el.getBoundingClientRect();
+//         left = props.left + sOff.x;
+//         top = props.top + sOff.y;
+//     } else { // for older browsers
+//         do {
+//             left += el.offsetLeft;
+//             top += el.offsetTop;
+//         } while ( (el = el.offsetParent) );
+//     }
+//     return Math.round(top) ;
+// }
+//
+
+function navBarFade() {
+  if(dw_getScrollOffsets().y > 500) {
+    document.getElementById('navbar').style.backgroundColor = '#222222'
+    document.getElementById('navbar').getElementsByTagName('img')[0].style.margin = '-30px 0 -5px 0px'
+  } else {
+    document.getElementById('navbar').style.backgroundColor = '#00000050'
+    document.getElementById('navbar').getElementsByTagName('img')[0].style.margin = '-30px 0 -5px -95px'
+  }
+
+  document.getElementById('navbar').getElementsByTagName('a')[0].getAttribute('')
+
+  for (var i = 0; i< document.getElementById('navbar').getElementsByTagName('a').length; i++) {
+    if(document.getElementById('navbar').getElementsByTagName('a')[i].getAttribute('href') == "") {
+      document.getElementById('navbar').getElementsByTagName('a')[i].style.color = 'white'
+    }
+  }
+
+  setTimeout(navBarFade,100)
+}
+
+function dw_getScrollOffsets() {
+    var doc = document, w = window;
+    var x, y, docEl;
+
+    if ( typeof w.pageYOffset === 'number' ) {
+        x = w.pageXOffset;
+        y = w.pageYOffset;
+    } else {
+        docEl = (doc.compatMode && doc.compatMode === 'CSS1Compat')?
+                doc.documentElement: doc.body;
+        x = docEl.scrollLeft;
+        y = docEl.scrollTop;
+    }
+    return {x:x, y:y};
+}
+
+function loadMapsList() {
+    var mapSection = "";
+    for (var map in mapsList) {
+        mapSection += "<a href='maps/" + mapsList[map]["url"] + ".html'> <div class='listItem'> <img src='" + mapsList[map]["icon"] + "'/> <div class='listItemDimmer'> <p class='listItemHeader_hiddenUntilHover'>" + mapsList[map]["title"] + "</p> <p class='listItemText_hiddenUntilHover'>" + mapsList[map]["shortDescription"] + "</div> </div> </a>";
+        console.log(fixMapDescription(parseInt(map)));
+    }
+    document.getElementsByClassName("sectionlistMaps")[0].innerHTML = mapSection;
+}
+function doMapPage(mapName) {
+    var mapPageData = "";
+    for (var map in mapsList) {
+        if (mapName == mapsList[map]["title"]) {
+            mapPageData += "<div class='breaker' style=\"height: 1000px; background-image: url('" + mapsList[map]["icon"] + "'); margin: -20px -10px 30px -10px\"> </div> <svg class='breakerEase' viewBox='0 0 200 10' style='transform: translate(0, -120%)'> <path transform='translate(0, 0)' d='M0 10 L0 0 Q100 10 200 0 Q300 10 400 0 Q500 10 600 0 Q700 10 800 0 Q900 10 1000 0 Q1100 10 1200 0 Q1300 10 1400 0 Q1500 10 1600 0 L1600 10 L200 10 Z ' fill='white'/> </svg> <div class='content'> <div class='section'> <span class='sectionheader'>" + mapsList[map]["title"] + "</span><br><a href='" + mapsList[map]["download"] + "'><button class='download'>Download </button> </a> <p class='sectiontext'>" + fixMapDescription(parseInt(map)) + "</p> </div>   <div class='breaker' style=\"height: 600px; background-image: url('" + mapsList[map]["images"][0] + "')\"> <svg class='breakerEase' viewBox='0 0 200 10'> <path d='     M0 0 Q100 10 200 0 Q300 10 400 0 Q500 10 600 0 Q700 10 800 0 Q900 10 1000 0 L0 0Z ' fill='white'/> </svg> </div> <svg class='breakerEase' viewBox='0 0 200 10' style='transform: translate(0, -120%)'> <path transform='translate(0, 0)' d='M0 10 L0 0 Q100 10 200 0 Q300 10 400 0 Q500 10 600 0 Q700 10 800 0 Q900 10 1000 0 Q1100 10 1200 0 Q1300 10 1400 0 Q1500 10 1600 0 L1600 10 L200 10 Z ' fill='white'/> </svg> <div class='section'>" + stringifyMapImages(map) + " </div> <div class='breaker' style=\"height: 600px; background-image: url('" + mapsList[map]["images"][mapsList[map]["images"].length-1] + "')\"> <svg class='breakerEase' viewBox='0 0 200 10'> <path d='     M0 0 Q100 10 200 0 Q300 10 400 0 Q500 10 600 0 Q700 10 800 0 Q900 10 1000 0 L0 0Z ' fill='white'/> </svg> </div> <svg class='breakerEase' viewBox='0 0 200 10' style='transform: translate(0, -120%)'> <path transform='translate(0, 0)' d='M0 10 L0 0 Q100 10 200 0 Q300 10 400 0 Q500 10 600 0 Q700 10 800 0 Q900 10 1000 0 Q1100 10 1200 0 Q1300 10 1400 0 Q1500 10 1600 0 L1600 10 L200 10 Z ' fill='white'/> </svg> <div class='section'> <span class='sectionheader'>Team Blocksanity</span> <div class='sectionlist'> <div class='listItem'> <p class=\"listItemHeader\"><a href='https://twitter.com/teamblocksanity'>Twitter</a></p> </div> <div class=\"listItem\"> <p class=\"listItemHeader\"><a href='https://www.youtube.com/channel/UCcJu68xme6GhjZgz6qNhwVg'>Youtube</p><a> </div> <div class=\"listItem\"> <p class=\"listItemHeader\"><a href='https://discord.gg/4eBnA9B'>Discord</a></p> </div> <footer>&copy; Copyright 2019 Team Blocksanity. Website by Benjamin Meier</footer> </div> </div></div>";
+        }
+    }
+    console.log(mapPageData)
+    document.getElementsByTagName("body")[0].innerHTML = mapPageData;
+}
+function fixMapDescription(map) {
+    var fixedDesc = "";
+    var desc = mapsList[map]["longDescription"];
+    if (desc.indexOf(')') > -1) {
+    while (desc.indexOf(')') > -1) {
+        var linkStart = desc.indexOf("(") + 1;
+        var linkEnd = desc.indexOf(")");
+        var tempDesc = desc.substring(linkStart, linkEnd);
+        if (tempDesc.indexOf("youtube") > -1) {
+            tempDesc = "<iframe width='655' height='398' src='" + tempDesc + "' frameborder='0' allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture' allowfullscreen=''></iframe>";
+        }
+        else if (tempDesc.indexOf('.png') > -1 || tempDesc.indexOf('.jpg') > -1) {
+            tempDesc = "<img src='" + tempDesc + "' alt='map image'/>";
+        }
+        else {
+            tempDesc = "<a href='" + tempDesc + "'>This shouldn't be here!/<a>";
+        }
+        fixedDesc += tempDesc;
+        desc = desc.substr(linkEnd + 1);
+    }
+    return fixedDesc;
+  } else {
+    return desc
+  }
+}
+function stringifyMapImages(map) {
+    var stringed = "";
+    for (var image in mapsList[map]["images"]) {
+        stringed += "<div class=listItem> <img src='" + mapsList[map]["images"][image] + "' alt='Map image'/>  </div>";
+    }
+    return stringed;
+}
